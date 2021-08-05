fb13d5e7d0997bf7cc8dfe70538e94efb3c44a64
FoxyTheOne <gartel.av@gmail.com>
2021-08-05 14:10:27 +0300

    Исправлена prog_6

--- src/ru/teachmeskills/lesson_2/prog_6.java
+++ src/ru/teachmeskills/lesson_2/prog_6.java
@@ -7,9 +7,12 @@ package ru.teachmeskills.lesson_2;
 
 public class prog_6 {
     public static void main(String[] args) {
+        int count = 0;
 
-        for (int i = 2; i <= 100; i = i + 2) {
-            System.out.println(i);
+        while (count <= 100) {
+            count++;
+            if ((count%2 != 0)) continue; // если число не делится на 2 без остатка, то возвращаемся в начало цикла
+            System.out.println(count); // если делится, то выводим их на экран
         }
 
     }
--- src/ru/teachmeskills/lesson_2/prog_7.java
+++ src/ru/teachmeskills/lesson_2/prog_7.java
@@ -9,11 +9,13 @@ public class prog_7 {
     public static void main(String[] args) {
         int sum = 0;
 
-        for (int i = 1; i < 100; i = i + 2) {
+        for (int i = 1; i < 100; i = i + 2) { // Считаем числа через один, начиная с 1 и суммируем их
             sum = sum + i;
         }
 
         System.out.println(sum);
+
+        // Можно ли решать эту задачу так, или следует сделать аналогично предыдущей?
 
     }
 }
